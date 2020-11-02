530479d4a5250feba94c96ae4ddcf47fd0617165
Ana Cristina <ana.henao8@udea.edu.co>
2020-11-02 07:32:16 -0500

    impresion de los numeros

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ Partendo de la caena de numeros, eparalos en dos cifras y sumarlas e imprimir el
 En caso de no poderse divir exactamente en dos cifras, poner ceros a la izquierda del primer numero*/
 using namespace std;
 // variabes globales y macros
-#define TAM (int)10
+#define TAM (int)10//reemplaza tam por int 10 donde se ponga tam
 
 
 int main()
@@ -13,6 +13,13 @@ int main()
     char a[TAM]="AB11CD44";
     for (int var=0; var<TAM ;var++){
         cout <<a[var] << endl;
+    }
+
+    cout << "imprimir solo los numeros de la cadena original" << endl;
+    for(int var=0; var<TAM; var++){
+        if(a[var]>='0' or a[var] <='9'){
+        cout << a[var] << endl;
+          }
     }
 
     return 0;
