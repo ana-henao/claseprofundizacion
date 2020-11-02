c3c95273703af122cfa76d8f8a95cb5157189fba
Ana Cristina <ana.henao8@udea.edu.co>
2020-11-02 07:13:51 -0500

    inicializacion variale que almacena la cadena de entrada e impresion caracter a caracter

--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@ solo con los caracteres y otra solo con numeros de la cadena original.
 Partendo de la caena de numeros, eparalos en dos cifras y sumarlas e imprimir el resultado.
 En caso de no poderse divir exactamente en dos cifras, poner ceros a la izquierda del primer numero*/
 using namespace std;
+// variabes globales y macros
+#define TAM (int)10
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    char a[TAM]="AB11CD44";
+    for (int var=0; var<TAM ;var++){
+        cout <<a[var] << endl;
+    }
+
     return 0;
 }
