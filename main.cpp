d12db64d06ee01fec55d8954f14cf7be736b299f
Ana Cristina <ana.henao8@udea.edu.co>
2020-11-02 08:56:34 -0500

    se separaron los digitos de la cadena original

--- main.cpp
+++ main.cpp
@@ -5,15 +5,27 @@ Partendo de la caena de numeros, eparalos en dos cifras y sumarlas e imprimir el
 En caso de no poderse divir exactamente en dos cifras, poner ceros a la izquierda del primer numero*/
 using namespace std;
 // variabes globales y macros
-#define TAM (int)10
+#define TAM (int)50
 
 
 int main()
 {
-    char a[TAM]="AB11CD44";
-    for (int var=0; var<TAM ;var++){
-        cout <<a[var] << endl;
-    }
+    char a[TAM]="AB11CD44EF5X5";
+    char numeros[TAM]={0};
+    int conDig=0;
 
+
+    cout << "imprimir solo los numeros de la cadena original"<< endl;
+    //int numero=0;
+    //int contadorDigitos=0;
+
+    for(int var=0; var<TAM ; ++var){
+        if(a[var]>='0' and a[var] <='9'){
+            numeros[conDig++]=a[var];
+        }
+    }
+    for (int var=conDig-1; var>=0; --var){
+        cout << numeros[var] << endl;
+    }
     return 0;
 }
